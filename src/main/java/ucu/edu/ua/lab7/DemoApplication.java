bca5423c7e60224e5e758d229a14e960c76efffd
Marharyta <paduchak.pn@ucu.edu.ua>
2025-11-01 23:52:39 +0200

    hi

--- src/main/java/ucu/edu/ua/lab7/DemoApplication.java
+++ src/main/java/ucu/edu/ua/lab7/DemoApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
